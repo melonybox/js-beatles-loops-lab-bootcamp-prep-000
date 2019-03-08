32709a8adb8e97bce2632bb903719b609b4932cb
Gino Desangles <blushingbox@yahoo.com>
2019-03-08 02:11:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 function theBeatlesPlay (musicians, instruments) {
   var array = []
   for (let i = musicians.length - musicians.length; i < musicians.length; i++ ) {
+    counter = 
     array.push(musicians + " plays " + instruments)
   }
   return array
